1c62843a4a7c4d5f1fff7b64b3387db336c64a74
crullian <crullian@gmail.com>
2014-12-09 13:26:23 -0500

    Get jQuery working

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
-var theNumber = Math.floor(Math.random() * 100) + 1;
-alert(theNumber);
-
-
 $(document).ready(function() {
-  // alert($(".submit").length);
-  //$("input").hide().show("slow");
+  var theNumber = Math.floor(Math.random() * 100) + 1;
 
-  $("#buttons").on("click", ".submit", function() {
-    $(".status").slideToggle();
+  console.log("The number is " + theNumber);
+
+  var message = "";
+
+  $('#userInput').keydown(function(event) {
+    if (event.keyCode == 13) {
+      $('.submit').trigger('click');
+    }
+  });
+
+  $("#buttons").on("click", ".submit", function(event) {
+    var userInput = +$("#userInput").val();
+
+    if (userInput != Math.floor(userInput) || userInput == "") {
+      message = "Please submit an integer!";
+
+    } else if (userInput === theNumber) {
+
+      message = "Correct!";
+    } else {
+      message = "Wrong, try again!"
+    }
+
+    $(".status").text(message).slideDown().fadeOut(2000);
+
+
+    event.preventDefault();
+  });
+  $("#buttons").on("click", ".hint", function() {
+    $(".status").text("the number is " + theNumber).slideDown().fadeOut(2000);
   })
-
 });
